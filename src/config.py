ea80291f37fe53e9c90ecfce70fbd0e2e1b5d777
adhitizki <adhitizki@gmail.com>
2026-09-15 12:14:43 +0700

    initial commit

---
+++ src/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DIR = DATA_DIR / "raw"
+INTERIM_DIR = DATA_DIR / "interim"
+PROCESSED_DIR = DATA_DIR / "processed"
+DOCS_DIR = PROJECT_ROOT / "docs"
